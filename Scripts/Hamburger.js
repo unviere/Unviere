69e232e174c68cc8d7835abf2ea73c78bbcff6fa
foodeggs7 <145258065+foodeggs7@users.noreply.github.com>
2024-06-21 15:56:07 +0200

    Update Hamburger.js

--- Scripts/Hamburger.js
+++ Scripts/Hamburger.js
@@ -1,7 +1,7 @@
-let menu = document.QeurySelector('#menu-icon');
-let navlist = document.QeurySelector('.navlist');
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-  menu.ClassList.toggle ('bx-x');
-  navlist.ClassList.toggle('open');
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('navlist-open'); // Make sure this class name matches the one in your CSS
 }
