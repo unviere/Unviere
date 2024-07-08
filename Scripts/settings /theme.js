67f1885b0ca7f4dba5cfdca7954ce0e94258ef71
unviere <foodeggs7@gmail.com>
2024-07-08 15:55:12 +0200

    update

---
+++ Scripts/settings /theme.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const defaultTheme = 'default';
+    const themes = ['dark-mode', 'light-mode'];
+
+    const currentTheme = localStorage.getItem('theme') || defaultTheme;
+    
+    if (currentTheme !== defaultTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
+    document.getElementById('unviere-light').addEventListener('click', () => {
+        switchTheme(defaultTheme);
+    });
+
+    document.getElementById('unviere-dark').addEventListener('click', () => {
+        switchTheme('dark-mode');
+    });
+
+    document.getElementById('unviere-light').addEventListener('click', () => {
+        switchTheme('light-mode');
+    });
+
+    
+
+    function switchTheme(theme) {
+        document.body.className = ''; // Remove all theme classes
+        if (theme !== defaultTheme) {
+            document.body.classList.add(theme);
+        }
+        localStorage.setItem('theme', theme);
+    }
+});
