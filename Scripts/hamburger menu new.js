28ad9c7813f47447abc2192741565bb9d47e368d
unviere <foodeggs7@gmail.com>
2024-09-09 19:55:12 +0200

    update

--- Scripts/hamburger menu new.js
+++ Scripts/hamburger menu new.js
@@ -2,6 +2,17 @@ let menu = document.querySelector('#menu-icon');
 let menuf = document.querySelector('.menu');
 
 menu.onclick = () => {
+  // Toggle the class on the menu icon
   menu.classList.toggle('bx-x');
-  menuf.classList.toggle('menu-open'); // Make sure this class name matches the one in your CSS
-}
+
+  // Get the computed display style of the menu element
+  const currentDisplay = window.getComputedStyle(menuf).display;
+
+  // Toggle display property between 'none' and 'flex'
+  if (currentDisplay === "none") {
+    menuf.style.display = "flex";
+  } else {
+    menuf.style.display = "none";
+  }
+  // Make sure this class name matches the one in your CSS
+};
